b723e16fca8c961c180515978048441cdc5687d2
Bruna-Fernandes <brunarenata.cont@gmail.com>
2020-06-14 13:01:57 -0300

    meu primeiro commit

---
+++ input-output.js
@@ -0,0 +1,14 @@
+//diferente desta
+
+/*Comentario de bloco
+pode ser assim
+em varias linhas
+*/
+
+
+
+var nome, idade;
+nome = prompt("Digite o seu nome")
+idade = prompt("Digite a sua idade")
+alert("O nome digitado foi:" + nome)
+alert("A idade digitada foi:" + idade)
